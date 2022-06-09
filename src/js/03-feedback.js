6f2c7648cc0b2a2c38c31bb01e67b014983e1b34
Sasha Hulko <hpalex555@gmail.com>
2022-06-09 13:56:46 +0300

    fixed

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,31 +1,54 @@
-import '../css/common.css';
-import '../css/03-feedback.css';
-import throttle from 'lodash.throttle';
+import throttle from "lodash.throttle";
 
-const feedbackForm = document.querySelector('.feedback-form');
-const FORM_DATA = 'feedback-form-state';
-const formData = JSON.parse(localStorage.getItem(FORM_DATA)) ?? {};
+const KEY_STORAGE = 'feedback - form - state';
+const formRef = document.querySelector('.feedback-form');
 
-feedbackForm.elements.email.value = formData.email ?? '';
-feedbackForm.elements.message.value = formData.message ?? '';
+formRef.addEventListener('submit', onFormSubmit);
+formRef.addEventListener('input', throttle(onTextInput,  1000));
 
-const inputHandler = e => {
-  formData[e.target.name] = e.target.value;
-  localStorage.setItem(FORM_DATA, JSON.stringify(formData));
-};
+populateText();
 
-const submitHandler = e => {
-  e.preventDefault();
+function onFormSubmit(event) {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const formData = new FormData(form);
+    const finalData = {};
+    for(const [key, value] of formData.entries()){
+    if (!value) {
+        alert("Все поля должны быть заполнены!!");
+        return;
+    }
+        finalData[key] = value;
+    }
+    console.log(finalData);
+    form.reset();
+    localStorage.removeItem(KEY_STORAGE);
+}
 
-  if (feedbackForm.elements.email.value == '' || feedbackForm.elements.message.value == '') {
-    return;
-  }
+function onTextInput(event) {
+    const { name, value } = event.target;
+    const parsedData = JSON.parse(localStorage.getItem(KEY_STORAGE));
+    if (parsedData) {
+        const formData = {
+        ...parsedData,
+        [name] : value,    
+        };
+        localStorage.setItem(KEY_STORAGE, JSON.stringify(formData))
+        } else {
+        const formData = {[name] : value, 
+        };
+        localStorage.setItem(KEY_STORAGE, JSON.stringify(formData))
+    }
+}
 
-  localStorage.removeItem(FORM_DATA);
+function populateText() {
+    const parsedData = JSON.parse(localStorage.getItem(KEY_STORAGE));
+    if (parsedData) {
+        const inputNames = Object.keys(parsedData);
+    inputNames.forEach(inputName => {
+        const input = formRef.elements[inputName];
+        input.value = parsedData[inputName];
+    });
+    }
 
-  e.currentTarget.reset();
-  console.log(formData);
-};
-
-feedbackForm.addEventListener('input', throttle(inputHandler, 500));
-feedbackForm.addEventListener('submit', submitHandler);
+}
